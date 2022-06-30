92495bba474072c07433ab61356732438b711c01
Mykah Bonnette <mykahwalker@Chances-Air.localdomain>
2022-06-30 14:28:36 -0600

    rollbar lab

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+
+app.use(express.json())
+
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '8b94afbc3fca4a87a9dab954caf82b62',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+rollbar.log("Hello world!");
